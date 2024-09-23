76388b688dc38a5877073a982fdaa5b3931e18cd
Brandon <rbrandon087@gmail.com>
2024-09-22 18:21:51 -0700

    switch my navbar file into its own folder named compoents

--- food/src/App.tsx
+++ food/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles.css";
 import "./App.css";
+import Navbar from "./components/Navbar";
 import truck from "./img/food tuck.jpg";
 import logo from "./img/tes2.jpg";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -68,65 +69,7 @@ const App: React.FunctionComponent<IAppProps> = ({}) => {
     <div className="bg-white">
       <main>
         {/*Nav bar info here*/}
-        <nav className="navbar bg-body-tertiary fixed-top bg-opacity-75 top-0 z-50">
-          <div className="container-fluid">
-            <a className="navbar-brand text-main" href="#">
-              Offcanvas navbar
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="offcanvas"
-              data-bs-target="#offcanvasNavbar"
-              aria-controls="offcanvasNavbar"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-              className="offcanvas offcanvas-end"
-              tabIndex={0}
-              id="offcanvasNavbar"
-              aria-labelledby="offcanvasNavbarLabel"
-            >
-              <div className="offcanvas-header">
-                <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
-                  Offcanvas
-                </h5>
-                <button
-                  type="button"
-                  className="btn-close"
-                  data-bs-dismiss="offcanvas"
-                  aria-label="Close"
-                ></button>
-              </div>
-              <div className="offcanvas-body">
-                <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                  <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">
-                      Home
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="#">
-                      Link
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="#">
-                      Link
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="#">
-                      Link
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </nav>
+        <Navbar name={""} />
         <div>
           {/* Hero card */}
           <div className="relative">
---
+++ food/src/components/Navbar.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+interface INavbarProps {
+  name: string;
+}
+
+const Navbar: React.FC<INavbarProps> = ({}) => {
+  return (
+    <div>
+      <nav className="navbar bg-body-tertiary fixed-top bg-opacity-75 top-0 z-50">
+        <div className="container-fluid">
+          <a className="navbar-brand text-main" href="#">
+            Offcanvas navbar
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasNavbar"
+            aria-controls="offcanvasNavbar"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="offcanvas offcanvas-end"
+            tabIndex={0}
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+          >
+            <div className="offcanvas-header">
+              <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+                Offcanvas
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="offcanvas-body">
+              <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Link
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Link
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Link
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div></div>
+    </div>
+  );
+};
+export default Navbar;
